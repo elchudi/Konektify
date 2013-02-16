10bd254d45f4f3739f15e4389333868557a38bb8
Gustavo Giudici <gustavogiudici@gmail.com>
2013-02-16 12:49:18 +0100

    noninteractive upgrade of distribution and packages installed

--- vagrant/shell/main.sh
+++ vagrant/shell/main.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
-apt-get -q -y install python-pip python-dev
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+UNAME=`uname -r`
+apt-get -q -y install python-pip python-dev dkms build-essential uuid-dev sqlite3 libsqlite3-dev python-software-properties python-setuptools python-dev vim linux-headers-$UNAME
 pip install pyzmq
-
