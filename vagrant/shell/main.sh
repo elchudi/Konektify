2676fb04861598cdd07e50b0a3d7c414f9a50d46
Gustavo Giudici <gustavogiudici@gmail.com>
2013-02-16 12:00:00 +0100

    shell commands added

--- vagrant/shell/main.sh
+++ vagrant/shell/main.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
-apt-get -q -y install python-pip
+apt-get -q -y install python-pip python-dev
+pip install pyzmq
 
