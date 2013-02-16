17f501792054d199571ca7c48868ecbac0734400
Gustavo Giudici <gustavogiudici@gmail.com>
2013-02-16 14:20:44 +0100

    add curl to install script

--- vagrant/shell/main.sh
+++ vagrant/shell/main.sh
@@ -3,5 +3,5 @@
 # in your basebox, this will manually install it at this point using apt or yum
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 UNAME=`uname -r`
-apt-get -q -y install python-pip python-dev dkms build-essential uuid-dev sqlite3 libsqlite3-dev python-software-properties python-setuptools python-dev vim linux-headers-$UNAME
+apt-get -q -y install python-pip python-dev dkms build-essential uuid-dev sqlite3 libsqlite3-dev python-software-properties python-setuptools python-dev vim linux-headers-$UNAME curl
 pip install pyzmq
